20669ed9ca8e643c3a130ac87c902e9afbf8f456
earthpatipon <eartyearth@gmail.com>
2018-04-11 22:01:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/earthpatipon/recipeschef/SignupActivity.java
@@ -0,0 +1,151 @@
+package com.example.earthpatipon.recipeschef;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignupActivity";
+
+    @BindView(R.id.username_input) EditText userNameInput;
+    @BindView(R.id.birthdate_input) EditText birthDateInput;
+    @BindView(R.id.email_input) EditText emailInput;
+    @BindView(R.id.password_input) EditText passWordInput;
+    @BindView(R.id.confirm_password_input) EditText confirmPasswordInput;
+    @BindView(R.id.button_register) Button registerButton;
+    @BindView(R.id.button_back) Button backButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                Intent intent = new Intent(SignupActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+    }
+
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        registerButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        String userName = userNameInput.getText().toString();
+        String birthDate = birthDateInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String password = passWordInput.getText().toString();
+        String confirmPassword = confirmPasswordInput.getText().toString();
+
+        // TODO: Implement your own signup logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignupSuccess() {
+        registerButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        registerButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String userName = userNameInput.getText().toString();
+        String birthDate = birthDateInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String password = passWordInput.getText().toString();
+        String confirmPassword = confirmPasswordInput.getText().toString();
+
+        if (userName.isEmpty() || userName.length() < 3) {
+            userNameInput.setError("at least 3 characters");
+            valid = false;
+        } else {
+            userNameInput.setError(null);
+        }
+
+        if (birthDate.isEmpty()) {
+            birthDateInput.setError("Enter Valid Birth Date");
+            valid = false;
+        } else {
+            birthDateInput.setError(null);
+        }
+
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailInput.setError("enter a valid email address");
+            valid = false;
+        } else {
+            emailInput.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            passWordInput.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            passWordInput.setError(null);
+        }
+
+        if (confirmPassword.isEmpty() || confirmPassword.length() < 4 || confirmPassword.length() > 10 || !(confirmPassword.equals(password))) {
+            confirmPasswordInput.setError("Password Do not match");
+            valid = false;
+        } else {
+            confirmPasswordInput.setError(null);
+        }
+
+        return valid;
+    }
+
+}
