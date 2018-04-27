afbbf2687cb575edb1fb6b70c3f7adb9520a4596
earthpatipon <eartyearth@gmail.com>
2018-04-27 13:42:18 +0700

    Recipe Activity

--- app/src/main/java/com/example/earthpatipon/recipeschef/RecipeActivity.java
+++ app/src/main/java/com/example/earthpatipon/recipeschef/RecipeActivity.java
@@ -4,23 +4,65 @@
 package com.example.earthpatipon.recipeschef;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.earthpatipon.recipeschef.database.AppDatabase;
 import com.example.earthpatipon.recipeschef.entity.Recipe;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class RecipeActivity extends AppCompatActivity {
 
-    private Recipe recipeCurrent;
+    @BindView(R.id.recipe_image) ImageView recipeImage;
+    @BindView(R.id.recipe_name) TextView recipeName;
+    @BindView(R.id.recipe_description) TextView recipeDesciption;
+    @BindView(R.id.recipe_difficulty) TextView recipeDifficulty;
+    @BindView(R.id.recipe_time) TextView recipeTime;
+    @BindView(R.id.recipe_serve) TextView recipeServe;
+    @BindView(R.id.recipe_ingredient) TextView recipeIngredient;
+    @BindView(R.id.recipe_instruction) TextView recipeInstruction;
+    @BindView(R.id.recipe_category) TextView recipeCategory;
+
+    private Recipe recipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+        ButterKnife.bind(this);
 
-        getRecipeOnClick();
+        getCurrentRecipe();
+
+        try {
+            Bitmap bitmap;
+            bitmap = BitmapFactory.decodeStream(new FileInputStream(new File(getFilesDir().getPath() + File.separator + "RecipeImages", recipe.getRecipeName() + ".png")));
+            recipeImage.setImageBitmap(bitmap);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        recipeName.setText(recipe.getRecipeName());
+        recipeDesciption.setText(recipe.getDescription());
+        recipeDifficulty.setText(recipe.getDifficulty());
+        recipeTime.setText(Integer.toString(recipe.getTime()));
+        recipeServe.setText(Integer.toString(recipe.getServe()));
+        recipeIngredient.setText(recipe.getIngredient());
+        recipeInstruction.setText(recipe.getInstruction());
+        recipeCategory.setText(recipe.getCategory());
     }
 
     @Override
@@ -32,7 +74,7 @@ public class RecipeActivity extends AppCompatActivity {
         //overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
     }
 
-    private void getRecipeOnClick(){
+    private void getCurrentRecipe(){
 
         String recipeName = "";
 
@@ -42,6 +84,8 @@ public class RecipeActivity extends AppCompatActivity {
             recipeName = intent.getStringExtra("NAME_KEY");
         }
 
-        recipeCurrent = AppDatabase.getInstance(getApplicationContext()).recipeDao().getRecipe(recipeName);
+        recipe = AppDatabase.getInstance(getApplicationContext()).recipeDao().getRecipe(recipeName);
     }
+
+
 }
